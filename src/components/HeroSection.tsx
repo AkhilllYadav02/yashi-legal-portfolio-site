80d1b9b756fa96fe6df288171fc20f2577939720
AkhilllYadav02 <akhilllyadav09@gmail.com>
2025-06-09 13:33:15 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
       {/* Background Pattern */}
       <div className="absolute inset-0 opacity-10">
         <div className="absolute inset-0" style={{
-          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+          backgroundImage: `url("https://media.licdn.com/dms/image/v2/D4E03AQFBk42ekckz8A/profile-displayphoto-shrink_800_800/B4EZcK3COhHsAc-/0/1748233890267?e=1755129600&v=beta&t=FpS5UDPJc1HaGNcJGgIkBShyfE-DNulLTMQ7ofIahWA")`,
         }} />
       </div>
       
